b4c30048bc53b8d00c1822dea5881b7a5c99b85f
daengdaenglee <gunho1020@gmail.com>
2020-05-22 15:02:51 +0900

    $$createSVGPoint 함수 테스트 작성

---
+++ src/createSVGPoint/createSVGPoint.spec.js
@@ -0,0 +1,30 @@
+import { createSVGWindow } from "svgdom";
+import { $$createSVGPoint } from "./createSVGPoint.index.js";
+
+describe(`$$createSVGPoint`, () => {
+  let $dummy_svg;
+
+  beforeEach(() => {
+    const window = createSVGWindow();
+    $dummy_svg = window.document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+  });
+
+  test(`will create SVGPoint with given coordinates`, () => {
+    const x = Math.round(Math.random() * 1000);
+    const y = Math.round(Math.random() * 1000);
+    const p = $$createSVGPoint($dummy_svg)({ x, y });
+
+    expect(p.x).toEqual(x);
+    expect(p.y).toEqual(y);
+  });
+
+  test("will use 0 as default coordinate", () => {
+    const p = $$createSVGPoint($dummy_svg)();
+
+    expect(p.x).toEqual(0);
+    expect(p.y).toEqual(0);
+  });
+});
