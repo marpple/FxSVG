e09c8d0f1640d98a0ade7202fe23a9e9bacd360f
daengdaenglee <gunho1020@gmail.com>
2020-05-22 15:57:42 +0900

    $$createSVGMatrix 테스트 작성

---
+++ src/createSVGMatrix/createSVGMatrix.spec.js
@@ -0,0 +1,39 @@
+import { createSVGWindow } from "svgdom";
+import { $$createSVGMatrix } from "./createSVGMatrix.index.js";
+
+describe(`$$createSVGMatrix`, () => {
+  let $dummy_svg;
+
+  beforeEach(() => {
+    $dummy_svg = createSVGWindow().document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+  });
+
+  test(`will create SVGMatrix with given values`, () => {
+    const [a, b, c, d, e, f] = [...Array(6)].map(() =>
+      Math.round(Math.random() * 1000)
+    );
+
+    const matrix = $$createSVGMatrix($dummy_svg)({ a, b, c, d, e, f });
+
+    expect(matrix.a).toEqual(a);
+    expect(matrix.b).toEqual(b);
+    expect(matrix.c).toEqual(c);
+    expect(matrix.d).toEqual(d);
+    expect(matrix.e).toEqual(e);
+    expect(matrix.f).toEqual(f);
+  });
+
+  test(`will create SVGMatrix {a: 1, b: 0, c: 0, d: 1, e: 0, f: 0} if there is no given values`, () => {
+    const matrix = $$createSVGMatrix($dummy_svg)();
+
+    expect(matrix.a).toEqual(1);
+    expect(matrix.b).toEqual(0);
+    expect(matrix.c).toEqual(0);
+    expect(matrix.d).toEqual(1);
+    expect(matrix.e).toEqual(0);
+    expect(matrix.f).toEqual(0);
+  });
+});
