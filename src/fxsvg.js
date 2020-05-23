4f7ae23b53baa9156ea14c9ca8063409e404945a
daengdaenglee <gunho1020@gmail.com>
2020-05-23 23:50:16 +0900

    커스텀 빌드 시스템 -> 기본 ES 모듈 소스파일 사용

---
+++ src/fxsvg.js
@@ -0,0 +1,62 @@
+import { $$appendRotateTransform } from "./appendRotateTransform/appendRotateTransform.index.js";
+import { $$appendTranslateTransform } from "./appendTranslateTransform/appendTranslateTransform.index.js";
+import { $$consolidateTransformList } from "./consolidateTransformList/consolidateTransformList.index.js";
+import { $$controlRotateTransform } from "./controlRotateTransform/controlRotateTransform.index.js";
+import { $$controlTranslateTransform } from "./controlTranslateTransform/controlTranslateTransform.index.js";
+import { $$createSVGMatrix } from "./createSVGMatrix/createSVGMatrix.index.js";
+import { $$createSVGPoint } from "./createSVGPoint/createSVGPoint.index.js";
+import { $$createSVGRect } from "./createSVGRect/createSVGRect.index.js";
+import { $$createSVGTransform } from "./createSVGTransform/createSVGTransform.index.js";
+import { $$createSVGTransformMatrix } from "./createSVGTransformMatrix/createSVGTransformMatrix.index.js";
+import { $$createSVGTransformRotate } from "./createSVGTransformRotate/createSVGTransformRotate.index.js";
+import { $$createSVGTransformScale } from "./createSVGTransformScale/createSVGTransformScale.index.js";
+import { $$createSVGTransformTranslate } from "./createSVGTransformTranslate/createSVGTransformTranslate.index.js";
+import { $$el } from "./el/el.index.js";
+import { $$els } from "./els/els.index.js";
+import { $$getAnimTransformList } from "./getAnimTransformList/getAnimTransformList.index.js";
+import { $$getBaseTransformList } from "./getBaseTransformList/getBaseTransformList.index.js";
+import { $$getBoxPoints } from "./getBoxPoints/getBoxPoints.index.js";
+import { $$getCenterPoint } from "./getCenterPoint/getCenterPoint.index.js";
+import { $$getSVG, $$setSVG } from "./getSetSVG/getSetSVG.index.js";
+import { $$initRotateTransform } from "./initRotateTransform/initRotateTransform.index.js";
+import { $$initTranslateTransform } from "./initTranslateTransform/initTranslateTransform.index.js";
+import { $$isRotateSVGTransform } from "./isRotateSVGTransform/isRotateSVGTransform.index.js";
+import { $$isScaleSVGTransform } from "./isScaleSVGTransform/isScaleSVGTransform.index.js";
+import { $$isTranslateSVGTransform } from "./isTranslateSVGTransform/isTranslateSVGTransform.index.js";
+import { $$mergeRotateTransform } from "./mergeRotateTransform/mergeRotateTransform.index.js";
+import { $$mergeTranslateTransform } from "./mergeTranslateTransform/mergeTranslateTransform.index.js";
+import { $$updateRotateTransform } from "./updateRotateTransform/updateRotateTransform.index.js";
+import { $$updateTranslateTransform } from "./updateTranslateTransform/updateTranslateTransform.index.js";
+
+export const FxSVG = {
+  appendRotateTransform: $$appendRotateTransform,
+  appendTranslateTransform: $$appendTranslateTransform,
+  consolidateTransformList: $$consolidateTransformList,
+  controlRotateTransform: $$controlRotateTransform,
+  controlTranslateTransform: $$controlTranslateTransform,
+  createSVGMatrix: $$createSVGMatrix,
+  createSVGPoint: $$createSVGPoint,
+  createSVGRect: $$createSVGRect,
+  createSVGTransform: $$createSVGTransform,
+  createSVGTransformMatrix: $$createSVGTransformMatrix,
+  createSVGTransformRotate: $$createSVGTransformRotate,
+  createSVGTransformScale: $$createSVGTransformScale,
+  createSVGTransformTranslate: $$createSVGTransformTranslate,
+  el: $$el,
+  els: $$els,
+  getAnimTransformList: $$getAnimTransformList,
+  getBaseTransformList: $$getBaseTransformList,
+  getBoxPoints: $$getBoxPoints,
+  getCenterPoint: $$getCenterPoint,
+  getSVG: $$getSVG,
+  setSVG: $$setSVG,
+  initRotateTransform: $$initRotateTransform,
+  initTranslateTransform: $$initTranslateTransform,
+  isRotateSVGTransform: $$isRotateSVGTransform,
+  isScaleSVGTransform: $$isScaleSVGTransform,
+  isTranslateSVGTransform: $$isTranslateSVGTransform,
+  mergeRotateTransform: $$mergeRotateTransform,
+  mergeTranslateTransform: $$mergeTranslateTransform,
+  updateRotateTransform: $$updateRotateTransform,
+  updateTranslateTransform: $$updateTranslateTransform,
+};
