c8bc9f654e509160b3822b077614426e9f783f58
daengdaenglee <gunho1020@gmail.com>
2020-05-25 12:11:13 +0900

    브라우저용 패키지 제공

--- README.md
+++ README.md
@@ -2,6 +2,52 @@
 
 Functional SVG Handling Library
 
+## Installation
+
+FxSVG는 ECMAScript Module 로 작성된 패키지입니다.
+
+다음과 같은 형태로 패키지를 제공합니다.
+
+- ECMAScript Module
+- 브라우저에서 사용할 수 있는 번들링 파일
+
+### ECMAScript Module
+
+```shell script
+npm install fxsvg
+```
+
+```javascript
+import { $$createSVGTransformTranslate } from "fxsvg";
+import FxSVG from "fxsvg";
+
+const $el = document.querySelector("svg rect");
+const transform = $$createSVGTransformTranslate({ tx: 10, ty: 20 });
+FxSVG.getBaseTransformList($el).initialize(transform);
+```
+
+### In a Browser
+
+FxSVG는 SVG, ECMAScript 6+ 표준을 따르는 모던 브라우저만 지원합니다.
+
+FxSVG는 `window` 객체의 `$$` property를 namespace로 사용합니다.
+
+```shell script
+npm install fxsvg
+```
+
+```html
+<script src="path/to/node_modules/fxsvg/dist/fxsvg.js"></script>
+```
+
+```javascript
+const { $$el } = $$;
+
+const $rect = $$el(`<rect x="10" y="10" width="100" height="100"></rect>`);
+const { controller } = $$.controlTranslateTransform()($rect);
+controller.append({ tx: 10 }).append({ ty: 10 }).end();
+```
+
 ## API
 
 [`<svg></svg>` 엘리먼트 커링](./svg_currying.md)
---
+++ bin/build.cjs
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const path = require("path");
+const { Readable } = require("stream");
+const webpack = require("webpack");
+
+const BROWSER_INDEX_FILENAME = "browser_index";
+
+(async () => {
+  fs.rmdirSync(path.resolve(__dirname, "../dist"), {
+    recursive: true,
+    maxRetries: 10,
+    retryDelay: 300,
+  });
+  fs.mkdirSync(path.resolve(__dirname, "../dist"));
+
+  const browser_index_js = `
+import $$ from "../src/index.js";
+window.$$ = $$;
+`.trimStart();
+  Readable.from([browser_index_js]).pipe(
+    fs.createWriteStream(path.resolve(__dirname, "../dist/browser_index.js"), {
+      flags: "w",
+    })
+  );
+
+  await new Promise((resolve, reject) =>
+    webpack(
+      {
+        mode: "production",
+        context: path.resolve(__dirname, "../"),
+        entry: `./dist/${BROWSER_INDEX_FILENAME}.js`,
+        output: {
+          filename: "fxsvg.js",
+          path: path.resolve(__dirname, "../dist"),
+        },
+      },
+      (error, stats) => {
+        if (error) {
+          return reject(error);
+        }
+
+        const info = stats.toJson("verbose");
+
+        if (stats.hasErrors()) {
+          const error = new Error(JSON.stringify(info, null, 2));
+          return reject(error);
+        }
+
+        resolve(info);
+      }
+    )
+  );
+
+  fs.unlinkSync(
+    path.resolve(__dirname, `../dist/${BROWSER_INDEX_FILENAME}.js`)
+  );
+})();
