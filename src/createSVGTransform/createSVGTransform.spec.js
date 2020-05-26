0830649be33c7a2cb47ffbbb1884a43980d90d75
daengdaenglee <gunho1020@gmail.com>
2020-05-26 16:53:35 +0900

    $$createSVGTransform 테스트 수정

--- src/createSVGTransform/createSVGTransform.spec.js
+++ src/createSVGTransform/createSVGTransform.spec.js
@@ -1,14 +1,16 @@
 import { expect } from "chai";
 import { $$createSVGTransform } from "./createSVGTransform.index.js";
 
-describe(`$$createSVGTransform`, () => {
-  it(`
-  will create SVGTransform
-  initialized to an identity matrix transform
-  `, () => {
+describe(`$$createSVGTransform`, function () {
+  it(`The return value will be a SVGTransform.`, function () {
     const t = $$createSVGTransform();
 
     expect(t).to.be.instanceof(SVGTransform);
+  });
+
+  it(`The SVGTransform will be initialized with an identity matrix.`, function () {
+    const t = $$createSVGTransform();
+
     expect(t.type).to.equal(SVGTransform.SVG_TRANSFORM_MATRIX);
     expect(t.matrix).to.include({ a: 1, b: 0, c: 0, d: 1, e: 0, f: 0 });
   });
