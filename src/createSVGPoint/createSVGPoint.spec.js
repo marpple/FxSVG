1184237921de92ebaa36a02a00d91595cf63d907
daengdaenglee <gunho1020@gmail.com>
2020-05-26 15:59:35 +0900

    $$createSVGPoint 테스트 수정

--- src/createSVGPoint/createSVGPoint.spec.js
+++ src/createSVGPoint/createSVGPoint.spec.js
@@ -1,32 +1,51 @@
 import { expect } from "chai";
 import { $$createSVGPoint } from "./createSVGPoint.index.js";
 
+const makeRandomNumber = () => {
+  const n = Math.round(Math.random() * 1000);
+  return Math.round(Math.random()) ? n : -n;
+};
+
+const makeTests = () => {
+  const l = [
+    {},
+    { x: makeRandomNumber() },
+    { y: makeRandomNumber() },
+    { x: makeRandomNumber(), y: makeRandomNumber() },
+  ];
+  return [
+    { point: $$createSVGPoint()() },
+    ...l.map((values) => ({ values, point: $$createSVGPoint()(values) })),
+    {
+      point: $$createSVGPoint(
+        document.createElementNS("http://www.w3.org/2000/svg", "svg")
+      )(),
+    },
+    ...l.map((values) => ({
+      values,
+      point: $$createSVGPoint(
+        document.createElementNS("http://www.w3.org/2000/svg", "svg")
+      )(values),
+    })),
+  ];
+};
+
 describe(`$$createSVGPoint`, () => {
-  it(`will create SVGPoint with given coordinates`, () => {
-    const x = Math.round(Math.random() * 1000);
-    const y = Math.round(Math.random() * 1000);
-    const p = $$createSVGPoint()({ x, y });
-
-    expect(p.x).to.equal(x);
-    expect(p.y).to.equal(y);
+  it(`The return value will be a SVGPoint`, () => {
+    const tests = makeTests();
+    for (const { point } of tests) {
+      expect(point).to.instanceof(SVGPoint);
+    }
   });
 
-  it("will use 0 as default coordinates", () => {
-    const p = $$createSVGPoint()();
-
-    expect(p.x).to.equal(0);
-    expect(p.y).to.equal(0);
-  });
-
-  it("will use 0 as default x coordinate", () => {
-    const p = $$createSVGPoint()({ y: 10 });
-
-    expect(p.x).to.equal(0);
-  });
-
-  it("will use 0 as default y coordinate", () => {
-    const p = $$createSVGPoint()({ x: 10 });
-
-    expect(p.y).to.equal(0);
+  it(`
+  The point's x, y value will be same with input value
+  The omitted value will be 0
+  `, () => {
+    const tests = makeTests();
+    for (const { point, values: { x = 0, y = 0 } = {} } of tests) {
+      expect(point.x).to.equal(x);
+      expect(point.y).to.equal(y);
+    }
   });
 });
