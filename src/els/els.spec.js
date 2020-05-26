668ad1a99e5f268126d49d406edb8623c8547269
daengdaenglee <gunho1020@gmail.com>
2020-05-26 17:39:25 +0900

    $$els 테스트 수정

--- src/els/els.spec.js
+++ src/els/els.spec.js
@@ -1,8 +1,8 @@
 import { expect } from "chai";
 import { $$els } from "./els.index.js";
 
-describe(`$$els`, () => {
-  it(`will return a list of "SVGElement"s using the input SVG string`, () => {
+describe(`$$els`, function () {
+  it(`The return value is a list of "SVGElement"s using the input SVG string.`, function () {
     const list = $$els()(`
       <rect x="0" y="0" width="10" height="10"></rect>
       <circle cx="10" cy="10" r="30"></circle> 
