21d908884016c441f569b295274c87fa834ff0c5
daengdaenglee <gunho1020@gmail.com>
2020-05-27 12:12:33 +0900

    테스트 서버 코드 단순화

--- test/server.cjs
+++ test/server.cjs
@@ -2,10 +2,21 @@ const path = require("path");
 const express = require("express");
 const webpack = require("webpack");
 const middleware = require("webpack-dev-middleware");
-const config = require("./webpack.test.config.cjs");
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, "../")));
-app.use("/webpack", middleware(webpack(config)));
+app.use(
+  "/test",
+  middleware(
+    webpack({
+      mode: "development",
+      context: __dirname,
+      entry: "./spec.js",
+      output: {
+        filename: "spec.bundle.js",
+      },
+    })
+  )
+);
 
 app.listen(8080, () => console.log("TEST SERVER LISTENING ON 8080 PORT"));
--- test/webpack.test.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mode: "development",
-  context: __dirname,
-  entry: "./fxsvg.spec.js",
-  output: {
-    filename: "fxsvg.spec.bundle.js",
-    path: __dirname,
-  },
-};
