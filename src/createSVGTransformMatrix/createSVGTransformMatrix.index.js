fa8bd079fc183a76ddec4cea053b4a4e281f764e
daengdaenglee <gunho1020@gmail.com>
2020-05-27 16:03:19 +0900

    $$createSVGTransformMatrix 에서 matrix 기본 인자 설정

--- src/createSVGTransformMatrix/createSVGTransformMatrix.index.js
+++ src/createSVGTransformMatrix/createSVGTransformMatrix.index.js
@@ -1,7 +1,10 @@
+import { $$createSVGMatrix } from "../createSVGMatrix/createSVGMatrix.index.js";
 import { $$createSVGTransform } from "../createSVGTransform/createSVGTransform.index.js";
 import { $$getSVG } from "../getSetSVG/getSetSVG.index.js";
 
-export const $$createSVGTransformMatrix = ($svg = $$getSVG()) => (matrix) => {
+export const $$createSVGTransformMatrix = ($svg = $$getSVG()) => (
+  matrix = $$createSVGMatrix($svg)()
+) => {
   const transform = $$createSVGTransform($svg);
   transform.setMatrix(matrix);
   return transform;
