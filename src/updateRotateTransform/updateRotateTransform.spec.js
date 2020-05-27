7c5ceb68ac99f905abc0ecf673277264fefc5adf
daengdaenglee <gunho1020@gmail.com>
2020-05-27 12:24:43 +0900

    $$updateRotateTransform 테스트 수정

--- src/updateRotateTransform/updateRotateTransform.spec.js
+++ src/updateRotateTransform/updateRotateTransform.spec.js
@@ -1,44 +1,94 @@
 import { expect } from "chai";
-import { makeRandomInt } from "../../test/utils/index.js";
+import { makeRandomInt, makeRandomNumber } from "../../test/utils/index.js";
+import { $$createSVGMatrix } from "../createSVGMatrix/createSVGMatrix.index.js";
+import { $$createSVGTransformMatrix } from "../createSVGTransformMatrix/createSVGTransformMatrix.index.js";
 import { $$createSVGTransformRotate } from "../createSVGTransformRotate/createSVGTransformRotate.index.js";
+import { $$createSVGTransformScale } from "../createSVGTransformScale/createSVGTransformScale.index.js";
+import { $$createSVGTransformTranslate } from "../createSVGTransformTranslate/createSVGTransformTranslate.index.js";
 import { $$updateRotateTransform } from "./updateRotateTransform.index.js";
 
 describe(`$$updateRotateTransform`, function () {
-  let t1;
-  let t2;
+  let t;
 
   beforeEach(function () {
-    t1 = $$createSVGTransformRotate()({
+    t = $$createSVGTransformRotate()({
       angle: makeRandomInt(),
       cx: 0,
       cy: 0,
-    });
-    t2 = $$createSVGTransformRotate()({
-      angle: makeRandomInt(),
-      cx: makeRandomInt(),
-      cy: makeRandomInt(),
     });
   });
 
   it(`The SVGTransform's angle value will be updated to the input value.`, function () {
     const angle = makeRandomInt();
-    $$updateRotateTransform(t1, { angle });
+    $$updateRotateTransform(t, { angle });
 
-    expect(t1.angle).to.equal(angle);
+    expect(t.angle).to.equal(angle);
   });
 
   it(`The SVGTransform's cx, cy values will be reset to 0.`, function () {
-    $$updateRotateTransform(t2, { angle: makeRandomInt() });
+    t = $$createSVGTransformRotate()({
+      angle: makeRandomInt(),
+      cx: makeRandomInt(),
+      cy: makeRandomInt(),
+    });
 
-    expect(t2.matrix.e).to.equal(0);
-    expect(t2.matrix.f).to.equal(0);
+    $$updateRotateTransform(t, { angle: makeRandomInt() });
+
+    expect(t.matrix.e).to.equal(0);
+    expect(t.matrix.f).to.equal(0);
   });
 
   it(`If no second arguments, the function will throw an error.`, function () {
-    expect(() => $$updateRotateTransform(t1)).to.throw();
+    expect(() => $$updateRotateTransform(t)).to.throw();
   });
 
   it(`If no angle value, the function will throw an error.`, function () {
-    expect(() => $$updateRotateTransform(t1, {})).to.throw();
+    expect(() => $$updateRotateTransform(t, {})).to.throw();
+  });
+
+  describe(`
+  If the SVGTransform's type is not a SVGTransform.SVG_TRANSFORM_ROTATE,
+  the function will do nothing but return SVGTransform. 
+  `, function () {
+    it(`Use a matrix transform.`, function () {
+      const matrix = $$createSVGMatrix()({
+        a: makeRandomNumber(),
+        b: makeRandomNumber(),
+        c: makeRandomNumber(),
+        d: makeRandomNumber(),
+        e: makeRandomNumber(),
+        f: makeRandomNumber(),
+      });
+      const matrix_t = $$createSVGTransformMatrix()(matrix);
+
+      expect(
+        $$updateRotateTransform(matrix_t, { angle: makeRandomNumber() })
+      ).to.equal(matrix_t);
+      expect(matrix_t.matrix).to.deep.equal(matrix);
+    });
+
+    it(`Use a translate transform.`, function () {
+      const translate_t = $$createSVGTransformTranslate()({
+        tx: makeRandomNumber(),
+        ty: makeRandomNumber(),
+      });
+      const { matrix } = translate_t;
+
+      expect($$updateRotateTransform(translate_t, { angle: makeRandomNumber() }));
+      expect(translate_t.matrix).to.deep.equal(matrix);
+    });
+
+    it(`Use a scale transform.`, function () {
+      const scale_t = $$createSVGTransformScale()({
+        sx: makeRandomNumber(),
+        sy: makeRandomNumber(),
+      });
+      const { matrix } = scale_t;
+
+      expect(
+        $$updateRotateTransform(scale_t, { angle: makeRandomNumber() })
+      ).to.equal(scale_t);
+      expect(scale_t.matrix).to.deep.equal(matrix);
+    });
   });
 });
