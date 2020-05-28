3fbda53cfdfb4ee5ca2ef2dd3e190ea854a79e58
daengdaenglee <gunho1020@gmail.com>
2020-05-28 17:56:04 +0900

    $$getCenterPoint 함수 테스트 작성

--- test/spec.js
+++ test/spec.js
@@ -14,6 +14,7 @@ import "../src/els/els.spec.js";
 import "../src/getAnimTransformList/getAnimTransformList.spec.js";
 import "../src/getBaseTransformList/getBaseTransformList.spec.js";
 import "../src/getBoxPoints/getBoxPoints.spec.js";
+import "../src/getCenterPoint/getCenterPoint.spec.js";
 import "../src/getSetSVG/getSetSVG.spec.js";
 import "../src/initRotateTransform/initRotateTransform.spec.js";
 import "../src/initTranslateTransform/initTranslateTransform.spec.js";
