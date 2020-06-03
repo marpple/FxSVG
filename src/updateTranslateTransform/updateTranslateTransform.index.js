b1c2bc1cee93060a3d526c74719e334025efe696
daengdaenglee <gunho1020@gmail.com>
2020-06-03 16:15:43 +0900

    $$updateTranslateTransform 코드 개선

--- src/updateTranslateTransform/updateTranslateTransform.index.js
+++ src/updateTranslateTransform/updateTranslateTransform.index.js
@@ -1,13 +1,13 @@
 import { $$isTranslateSVGTransform } from "../isTranslateSVGTransform/isTranslateSVGTransform.index.js";
 
-export const $$updateTranslateTransform = (transform, { tx, ty } = {}) => {
+export const $$updateTranslateTransform = (
+  transform,
+  { tx = transform.matrix.e, ty = transform.matrix.f } = {}
+) => {
   if (!$$isTranslateSVGTransform(transform)) {
     return transform;
   }
 
-  transform.setTranslate(
-    tx == null ? transform.matrix.e : tx,
-    ty == null ? transform.matrix.f : ty
-  );
+  transform.setTranslate(tx, ty);
   return transform;
 };
