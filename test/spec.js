9a2da966ab41e862f15b65aeb27fa784f0eaebfa
daengdaenglee <gunho1020@gmail.com>
2020-06-05 17:12:13 +0900

    $$mergeScaleTransform 테스트 작성

--- test/spec.js
+++ test/spec.js
@@ -28,6 +28,7 @@ import "../src/isScaleSVGTransform/isScaleSVGTransform.spec.js";
 import "../src/isTranslateSVGTransform/isTranslateSVGTransform.spec.js";
 import "../src/isValidFxScaleSVGTransformList/isValidFxScaleSVGTransformList.spec.js";
 import "../src/mergeRotateTransform/mergeRotateTransform.spec.js";
+import "../src/mergeScaleTransform/mergeScaleTransform.spec.js";
 import "../src/mergeTranslateTransform/mergeTranslateTransform.spec.js";
 import "../src/updateMatrixTransform/updateMatrixTransform.spec.js";
 import "../src/updateRotateTransform/updateRotateTransform.spec.js";
