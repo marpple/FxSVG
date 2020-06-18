561fce73dbfa67409496a7ff03630302d180ff66
daengdaenglee <gunho1020@gmail.com>
2020-06-18 20:52:05 +0900

    $$createSVGTransformMatrix 테스트 정리

--- src/createSVGTransformMatrix/createSVGTransformMatrix.spec.js
+++ src/createSVGTransformMatrix/createSVGTransformMatrix.spec.js
@@ -1,65 +1,84 @@
 import { expect } from "chai";
-import { each, flatMapL, go, mapL } from "fxjs2";
-import { makeRandomSVGMatrix } from "../../test/utils/index.js";
+import {
+  appendL,
+  defaultTo,
+  each,
+  extend,
+  flatMapL,
+  go,
+  mapL,
+  object,
+  pipe,
+} from "fxjs2";
+import { expectSameValueSVGMatrix } from "../../test/assertions/index.js";
+import {
+  makeAllCombinations,
+  makeRandomNumber,
+  makeRandomSVGMatrix,
+} from "../../test/utils/index.js";
 import { $$createSVGMatrix } from "../createSVGMatrix/createSVGMatrix.index.js";
 import { $$createSVGTransformMatrix } from "./createSVGTransformMatrix.index.js";
+
+const makeCases = () =>
+  flatMapL(
+    (f) =>
+      go(
+        ["matrix"],
+        makeAllCombinations,
+        mapL(
+          pipe(
+            mapL((k) => [
+              k,
+              makeRandomSVGMatrix(() => makeRandomNumber(-100, 100)),
+            ]),
+            object
+          )
+        ),
+        mapL((values) => ({ values, transform: f(values) })),
+        appendL({ transform: f() })
+      ),
+    [
+      $$createSVGTransformMatrix(),
+      $$createSVGTransformMatrix(
+        document.createElementNS("http://www.w3.org/2000/svg", "svg")
+      ),
+    ]
+  );
 
 export default ({ describe, it }) => [
   describe(`$$createSVGTransformMatrix`, function () {
     it(`The return value is a SVGTransform.`, function () {
       go(
-        [
-          $$createSVGTransformMatrix(),
-          $$createSVGTransformMatrix(
-            document.createElementNS("http://www.w3.org/2000/svg", "svg")
-          ),
-        ],
-        flatMapL((f) => [f(), f({}), f({ matrix: makeRandomSVGMatrix() })]),
-        each((t) => expect(t).to.instanceof(SVGTransform))
+        makeCases(),
+        mapL(({ transform: t }) => t),
+        each((transform) => expect(transform).to.instanceof(SVGTransform))
       );
     });
 
-    it(`The SVGTransform's type should be the SVGTransform.SVG_TRANSFORM_MATRIX.`, function () {
+    it(`The transform's type is the SVGTransform.SVG_TRANSFORM_MATRIX.`, function () {
       go(
-        [
-          $$createSVGTransformMatrix(),
-          $$createSVGTransformMatrix(
-            document.createElementNS("http://www.w3.org/2000/svg", "svg")
-          ),
-        ],
-        flatMapL((f) => [f(), f({}), f({ matrix: makeRandomSVGMatrix() })]),
-        mapL(({ type: t }) => t),
-        each((t) => expect(t).to.equal(SVGTransform.SVG_TRANSFORM_MATRIX))
+        makeCases(),
+        mapL(({ transform: t }) => t),
+        mapL(({ type }) => type),
+        each((type) => expect(type).to.equal(SVGTransform.SVG_TRANSFORM_MATRIX))
       );
     });
 
-    it(`The SVGTransform will be initialized with the given matrix.`, function () {
-      const m = makeRandomSVGMatrix();
+    it(`The transform is initialized with the given matrix.
+        If there is omitted values or no argument, the transform is initialized with an identity matrix.`, function () {
       go(
-        [
-          $$createSVGTransformMatrix(),
-          $$createSVGTransformMatrix(
-            document.createElementNS("http://www.w3.org/2000/svg", "svg")
-          ),
-        ],
-        mapL((f) => f({ matrix: m })),
-        mapL(({ matrix: m }) => m),
-        each((_m) => expect(_m).to.deep.equal(m))
-      );
-    });
-
-    it(`The SVGTransform will be initialized with identity matrix if there is no passed matrix.`, function () {
-      const identity_m = $$createSVGMatrix()();
-      go(
-        [
-          $$createSVGTransformMatrix(),
-          $$createSVGTransformMatrix(
-            document.createElementNS("http://www.w3.org/2000/svg", "svg")
-          ),
-        ],
-        flatMapL((f) => [f(), f({})]),
-        mapL(({ matrix: m }) => m),
-        each((m) => expect(m).to.deep.equal(identity_m))
+        makeCases(),
+        mapL(({ transform, values }) =>
+          go(
+            values,
+            defaultTo({}),
+            (values) => extend({ matrix: $$createSVGMatrix()() }, values),
+            (values) => ({ values, transform })
+          )
+        ),
+        each(({ transform, values: { matrix } }) =>
+          expectSameValueSVGMatrix(transform.matrix, matrix)
+        )
       );
     });
   }),
