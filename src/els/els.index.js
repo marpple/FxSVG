467ab336e66e1a70486ee7141be92431b92b23d9
daengdaenglee <gunho1020@gmail.com>
2020-06-19 15:51:57 +0900

    $$els 수정 및 테스트 수정

--- src/els/els.index.js
+++ src/els/els.index.js
@@ -1,7 +1,7 @@
 import { each } from "fxjs2";
 import { $$getSVG } from "../getSetSVG/getSetSVG.index.js";
 
-export const $$els = ($svg = $$getSVG()) => (svg) => {
+export const $$els = ($svg = $$getSVG()) => (svg = "") => {
   svg = svg.trim();
   $svg.innerHTML = svg;
   const result = each(($el) => $svg.removeChild($el), [...$svg.children]);
--- src/els/els.spec.js
+++ src/els/els.spec.js
@@ -1,28 +1,98 @@
 import { expect } from "chai";
+import {
+  entriesL,
+  equals2,
+  flatMapL,
+  go,
+  go1,
+  join,
+  map,
+  mapL,
+  rangeL,
+  zip,
+} from "fxjs2";
+import { makeRandomInt } from "../../test/utils/makeRandomInt.js";
 import { $$els } from "./els.index.js";
+
+const setupMock = ({ length = makeRandomInt(1) } = {}) =>
+  go(
+    rangeL(length),
+    mapL(() => makeRandomInt(0, 3)),
+    map((flag) => {
+      if (equals2(flag, 1)) {
+      }
+
+      if (equals2(flag, 2)) {
+      }
+
+      const name = "rect";
+      const [x, y] = go(
+        rangeL(2),
+        mapL(() => makeRandomInt(-100, 100)),
+        mapL((n) => `${n}`)
+      );
+      const [width, height] = go(
+        rangeL(2),
+        mapL(() => makeRandomInt(1)),
+        mapL((n) => `${n}`)
+      );
+      return {
+        name,
+        attrs: { x, y, width, height },
+        str: `<${name} x="${x}" y="${y}" width="${width}" height="${height}"></${name}>`,
+      };
+    })
+  );
 
 export default ({ describe, it }) => [
   describe(`$$els`, function () {
-    it(`The return value is a list of "SVGElement"s using the input SVG string.`, function () {
-      const list = $$els()(`
-      <rect x="0" y="0" width="10" height="10"></rect>
-      <circle cx="10" cy="10" r="30"></circle> 
-    `);
+    it(`The return value is a list of SVG elements using the input SVG string.`, function () {
+      this.slow(500);
 
-      expect(list).to.have.lengthOf(2);
+      const cases = go(
+        [
+          $$els(),
+          $$els(document.createElementNS("http://www.w3.org/2000/svg", "svg")),
+        ],
+        mapL((f) => {
+          const list = setupMock();
+          const $list_el = go(
+            list,
+            mapL(({ str: s }) => s),
+            join("\n"),
+            (str) => f(str)
+          );
+          return { list, $list_el };
+        })
+      );
 
-      expect(list[0]).to.be.instanceof(SVGElement);
-      expect(list[0].nodeName.toLowerCase()).to.equal("rect");
-      expect(list[0].getAttributeNS(null, "x")).to.equal("0");
-      expect(list[0].getAttributeNS(null, "y")).to.equal("0");
-      expect(list[0].getAttributeNS(null, "width")).to.equal("10");
-      expect(list[0].getAttributeNS(null, "height")).to.equal("10");
+      for (const { list, $list_el } of cases) {
+        expect($list_el.length).to.equal(list.length);
 
-      expect(list[1]).to.be.instanceof(SVGElement);
-      expect(list[1].nodeName.toLowerCase()).to.equal("circle");
-      expect(list[1].getAttributeNS(null, "cx")).to.equal("10");
-      expect(list[1].getAttributeNS(null, "cy")).to.equal("10");
-      expect(list[1].getAttributeNS(null, "r")).to.equal("30");
+        for (const [{ name, attrs }, $el] of zip(list, $list_el)) {
+          expect($el).to.instanceof(SVGElement);
+          expect($el.nodeName.toLowerCase()).to.equal(name);
+          for (const [key, expect_value] of entriesL(attrs)) {
+            const receive_value = $el.getAttributeNS(null, key);
+            expect(receive_value).to.equal(expect_value);
+          }
+        }
+      }
+    });
+
+    it(`The return value is a empty list
+        when the input SVG string is empty string or there is no input SVG string.`, function () {
+      const cases = go1(
+        [
+          $$els(),
+          $$els(document.createElementNS("http://www.w3.org/2000/svg", "svg")),
+        ],
+        flatMapL((f) => mapL((input) => f(input), ["", undefined]))
+      );
+
+      for (const $list_el of cases) {
+        expect($list_el.length).to.equal(0);
+      }
     });
   }),
 ];
