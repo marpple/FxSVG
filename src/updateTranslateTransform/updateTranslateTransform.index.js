b9b93b633c291a854e60b0bb879588ef5ad057a1
daengdaenglee <gunho1020@gmail.com>
2020-06-24 20:47:03 +0900

    $$updateTranslateTransform interface 변경 초안

--- src/updateTranslateTransform/updateTranslateTransform.index.js
+++ src/updateTranslateTransform/updateTranslateTransform.index.js
@@ -1,3 +1,4 @@
+import { defaultTo } from "fxjs2";
 import { $$isTranslateSVGTransform } from "../isTranslateSVGTransform/isTranslateSVGTransform.index.js";
 
 export const $$updateTranslateTransform = (
@@ -9,5 +10,29 @@ export const $$updateTranslateTransform = (
   }
 
   transform.setTranslate(tx, ty);
+  return transform;
+};
+
+export const $$updateTranslateTransform2 = ({ tx, ty } = {}) => (transform) => {
+  if (!$$isTranslateSVGTransform(transform)) {
+    return transform;
+  }
+
+  transform.setTranslate(
+    defaultTo(transform.matrix.e, tx),
+    defaultTo(transform.matrix.f, ty)
+  );
+  return transform;
+};
+
+export const $$updateTranslateTransform3 = ({ tx, ty } = {}, transform) => {
+  if (!$$isTranslateSVGTransform(transform)) {
+    return transform;
+  }
+
+  transform.setTranslate(
+    defaultTo(transform.matrix.e, tx),
+    defaultTo(transform.matrix.f, ty)
+  );
   return transform;
 };
--- src/updateTranslateTransform/updateTranslateTransform.spec.js
+++ src/updateTranslateTransform/updateTranslateTransform.spec.js
@@ -10,7 +10,11 @@ import { $$createSVGTransformMatrix } from "../createSVGTransformMatrix/createSV
 import { $$createSVGTransformRotate } from "../createSVGTransformRotate/createSVGTransformRotate.index.js";
 import { $$createSVGTransformScale } from "../createSVGTransformScale/createSVGTransformScale.index.js";
 import { $$createSVGTransformTranslate } from "../createSVGTransformTranslate/createSVGTransformTranslate.index.js";
-import { $$updateTranslateTransform } from "./updateTranslateTransform.index.js";
+import {
+  $$updateTranslateTransform,
+  $$updateTranslateTransform2,
+  $$updateTranslateTransform3,
+} from "./updateTranslateTransform.index.js";
 
 const setupMockTransform = () => {
   const [tx, ty] = mapL(() => makeRandomInt(-100, 100), rangeL(2));
@@ -26,80 +30,261 @@ const setupMockInputValues = () => {
 export default ({ describe, it }) => [
   describe(`$$updateTranslateTransform`, function () {
     it(`The tx, ty of the input transform is changed to input tx, ty.`, function () {
-      const { transform } = setupMockTransform();
-      const { tx, ty } = setupMockInputValues();
-      $$updateTranslateTransform(transform, { tx, ty });
+      const { transform: transform1 } = setupMockTransform();
+      const { tx: tx1, ty: ty1 } = setupMockInputValues();
+      $$updateTranslateTransform(transform1, { tx: tx1, ty: ty1 });
 
-      expectTransformWithTranslateTxTy({ transform, tx, ty });
+      expectTransformWithTranslateTxTy({
+        transform: transform1,
+        tx: tx1,
+        ty: ty1,
+      });
+
+      const { transform: transform2 } = setupMockTransform();
+      const { tx: tx2, ty: ty2 } = setupMockInputValues();
+      $$updateTranslateTransform2({ tx: tx2, ty: ty2 })(transform2);
+
+      expectTransformWithTranslateTxTy({
+        transform: transform2,
+        tx: tx2,
+        ty: ty2,
+      });
+
+      const { transform: transform3 } = setupMockTransform();
+      const { tx: tx3, ty: ty3 } = setupMockInputValues();
+      $$updateTranslateTransform3({ tx: tx3, ty: ty3 }, transform3);
+
+      expectTransformWithTranslateTxTy({
+        transform: transform3,
+        tx: tx3,
+        ty: ty3,
+      });
     });
 
     it(`The tx of the transform is same with before when there is no input tx.`, function () {
-      const { transform, tx } = setupMockTransform();
-      const { ty } = setupMockInputValues();
-      $$updateTranslateTransform(transform, { ty });
+      const { transform: transform1, tx: tx1 } = setupMockTransform();
+      const { ty: ty1 } = setupMockInputValues();
+      $$updateTranslateTransform(transform1, { ty: ty1 });
 
-      expectTransformWithTranslateTxTy({ transform, tx, ty });
+      expectTransformWithTranslateTxTy({
+        transform: transform1,
+        tx: tx1,
+        ty: ty1,
+      });
+
+      const { transform: transform2, tx: tx2 } = setupMockTransform();
+      const { ty: ty2 } = setupMockInputValues();
+      $$updateTranslateTransform2({ ty: ty2 })(transform2);
+
+      expectTransformWithTranslateTxTy({
+        transform: transform2,
+        tx: tx2,
+        ty: ty2,
+      });
+
+      const { transform: transform3, tx: tx3 } = setupMockTransform();
+      const { ty: ty3 } = setupMockInputValues();
+      $$updateTranslateTransform3({ ty: ty3 }, transform3);
+
+      expectTransformWithTranslateTxTy({
+        transform: transform3,
+        tx: tx3,
+        ty: ty3,
+      });
     });
 
     it(`The ty of the transform is same with before when there is no input ty.`, function () {
-      const { transform, ty } = setupMockTransform();
-      const { tx } = setupMockInputValues();
-      $$updateTranslateTransform(transform, { tx });
+      const { transform: transform1, ty: ty1 } = setupMockTransform();
+      const { tx: tx1 } = setupMockInputValues();
+      $$updateTranslateTransform(transform1, { tx: tx1 });
 
-      expectTransformWithTranslateTxTy({ transform, tx, ty });
+      expectTransformWithTranslateTxTy({
+        transform: transform1,
+        tx: tx1,
+        ty: ty1,
+      });
+
+      const { transform: transform2, ty: ty2 } = setupMockTransform();
+      const { tx: tx2 } = setupMockInputValues();
+      $$updateTranslateTransform2({ tx: tx2 })(transform2);
+
+      expectTransformWithTranslateTxTy({
+        transform: transform2,
+        tx: tx2,
+        ty: ty2,
+      });
+
+      const { transform: transform3, ty: ty3 } = setupMockTransform();
+      const { tx: tx3 } = setupMockInputValues();
+      $$updateTranslateTransform3({ tx: tx3 }, transform3);
+
+      expectTransformWithTranslateTxTy({
+        transform: transform3,
+        tx: tx3,
+        ty: ty3,
+      });
     });
 
     it(`The tx, ty of the transform is same with before when there is no input object.`, function () {
-      const { transform, tx, ty } = setupMockTransform();
-      $$updateTranslateTransform(transform);
+      const { transform: transform1, tx: tx1, ty: ty1 } = setupMockTransform();
+      $$updateTranslateTransform(transform1);
 
-      expectTransformWithTranslateTxTy({ transform, tx, ty });
+      expectTransformWithTranslateTxTy({
+        transform: transform1,
+        tx: tx1,
+        ty: ty1,
+      });
+
+      const { transform: transform2, tx: tx2, ty: ty2 } = setupMockTransform();
+      $$updateTranslateTransform2()(transform2);
+
+      expectTransformWithTranslateTxTy({
+        transform: transform2,
+        tx: tx2,
+        ty: ty2,
+      });
+
+      const { transform: transform3, tx: tx3, ty: ty3 } = setupMockTransform();
+      $$updateTranslateTransform3(undefined, transform3);
+
+      expectTransformWithTranslateTxTy({
+        transform: transform3,
+        tx: tx3,
+        ty: ty3,
+      });
     });
 
     describe(`If the transform is another type transform, the function will do nothing but return the input.`, function () {
       it(`When the transform is a matrix transform...`, function () {
-        const before_t = $$createSVGTransformMatrix()({
+        const before_t1 = $$createSVGTransformMatrix()({
           matrix: makeRandomSVGMatrix(() => makeRandomNumber(-100, 100)),
         });
 
-        const after_t = $$updateTranslateTransform(before_t, {
+        const after_t1 = $$updateTranslateTransform(before_t1, {
           tx: makeRandomNumber(-100, 100),
           ty: makeRandomNumber(-100, 100),
         });
 
-        expect(after_t).to.equal(before_t);
-        expect(after_t).to.deep.equal(before_t);
+        expect(after_t1).to.equal(before_t1);
+        expect(after_t1).to.deep.equal(before_t1);
+
+        const before_t2 = $$createSVGTransformMatrix()({
+          matrix: makeRandomSVGMatrix(() => makeRandomNumber(-100, 100)),
+        });
+
+        const after_t2 = $$updateTranslateTransform2({
+          tx: makeRandomNumber(-100, 100),
+          ty: makeRandomNumber(-100, 100),
+        })(before_t2);
+
+        expect(after_t2).to.equal(before_t2);
+        expect(after_t2).to.deep.equal(before_t2);
+
+        const before_t3 = $$createSVGTransformMatrix()({
+          matrix: makeRandomSVGMatrix(() => makeRandomNumber(-100, 100)),
+        });
+
+        const after_t3 = $$updateTranslateTransform3(
+          {
+            tx: makeRandomNumber(-100, 100),
+            ty: makeRandomNumber(-100, 100),
+          },
+          before_t3
+        );
+
+        expect(after_t3).to.equal(before_t3);
+        expect(after_t3).to.deep.equal(before_t3);
       });
 
       it(`When the transform is a rotate transform...`, function () {
-        const before_t = $$createSVGTransformRotate()({
+        const before_t1 = $$createSVGTransformRotate()({
           angle: makeRandomNumber(-700, 700),
           cx: makeRandomNumber(-100, 100),
           cy: makeRandomNumber(-100, 100),
         });
 
-        const after_t = $$updateTranslateTransform(before_t, {
+        const after_t1 = $$updateTranslateTransform(before_t1, {
           tx: makeRandomNumber(-100, 100),
           ty: makeRandomNumber(-100, 100),
         });
 
-        expect(after_t).to.equal(before_t);
-        expect(after_t).to.deep.equal(before_t);
+        expect(after_t1).to.equal(before_t1);
+        expect(after_t1).to.deep.equal(before_t1);
+
+        const before_t2 = $$createSVGTransformRotate()({
+          angle: makeRandomNumber(-700, 700),
+          cx: makeRandomNumber(-100, 100),
+          cy: makeRandomNumber(-100, 100),
+        });
+
+        const after_t2 = $$updateTranslateTransform2({
+          tx: makeRandomNumber(-100, 100),
+          ty: makeRandomNumber(-100, 100),
+        })(before_t2);
+
+        expect(after_t2).to.equal(before_t2);
+        expect(after_t2).to.deep.equal(before_t2);
+
+        const before_t3 = $$createSVGTransformRotate()({
+          angle: makeRandomNumber(-700, 700),
+          cx: makeRandomNumber(-100, 100),
+          cy: makeRandomNumber(-100, 100),
+        });
+
+        const after_t3 = $$updateTranslateTransform3(
+          {
+            tx: makeRandomNumber(-100, 100),
+            ty: makeRandomNumber(-100, 100),
+          },
+          before_t3
+        );
+
+        expect(after_t3).to.equal(before_t3);
+        expect(after_t3).to.deep.equal(before_t3);
       });
 
       it(`When the transform is a scale transform...`, function () {
-        const before_t = $$createSVGTransformScale()({
+        const before_t1 = $$createSVGTransformScale()({
           sx: makeRandomNumber(-100, 100),
           sy: makeRandomNumber(-100, 100),
         });
 
-        const after_t = $$updateTranslateTransform(before_t, {
+        const after_t1 = $$updateTranslateTransform(before_t1, {
           tx: makeRandomNumber(-100, 100),
           ty: makeRandomNumber(-100, 100),
         });
 
-        expect(after_t).to.equal(before_t);
-        expect(after_t).to.deep.equal(before_t);
+        expect(after_t1).to.equal(before_t1);
+        expect(after_t1).to.deep.equal(before_t1);
+
+        const before_t2 = $$createSVGTransformScale()({
+          sx: makeRandomNumber(-100, 100),
+          sy: makeRandomNumber(-100, 100),
+        });
+
+        const after_t2 = $$updateTranslateTransform2({
+          tx: makeRandomNumber(-100, 100),
+          ty: makeRandomNumber(-100, 100),
+        })(before_t2);
+
+        expect(after_t2).to.equal(before_t2);
+        expect(after_t2).to.deep.equal(before_t2);
+
+        const before_t3 = $$createSVGTransformScale()({
+          sx: makeRandomNumber(-100, 100),
+          sy: makeRandomNumber(-100, 100),
+        });
+
+        const after_t3 = $$updateTranslateTransform3(
+          {
+            tx: makeRandomNumber(-100, 100),
+            ty: makeRandomNumber(-100, 100),
+          },
+          before_t3
+        );
+
+        expect(after_t3).to.equal(before_t3);
+        expect(after_t3).to.deep.equal(before_t3);
       });
     });
   }),
