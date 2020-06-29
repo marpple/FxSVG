a375cdaa470976875e6f625c196f9ec729ddaecc
daengdaenglee <gunho1020@gmail.com>
2020-06-29 16:15:20 +0900

    $$mergeScaleTransform2 함수 interface 수정 초안

--- src/mergeScaleTransform2/mergeScaleTransform2.index.js
+++ src/mergeScaleTransform2/mergeScaleTransform2.index.js
@@ -1,4 +1,4 @@
-import { each, go, go1, mapL, rangeL, some } from "fxjs2";
+import { curry, each, go, go1, mapL, rangeL, some } from "fxjs2";
 import { $$getBaseTransformList } from "../getBaseTransformList/getBaseTransformList.index.js";
 import { $$isValidFxScaleSVGTransformList } from "../isValidFxScaleSVGTransformList/isValidFxScaleSVGTransformList.index.js";
 
@@ -68,3 +68,132 @@ export const $$mergeScaleTransform2 = (
 
   return $el;
 };
+
+export const $$mergeScaleTransform22 = ({
+  index = 1,
+  is_need_correction = true,
+  x_name = "x",
+  y_name = "y",
+  width_name = "width",
+  height_name = "height",
+  direction,
+} = {}) => ($el) => {
+  const transform_list = $$getBaseTransformList($el);
+
+  if (
+    !$$isValidFxScaleSVGTransformList(transform_list, { index }) ||
+    !VALID_DIRECTION.has(direction)
+  ) {
+    return $el;
+  }
+
+  const [{ e: cx2, f: cy2 }, { a: sx, d: sy }, { e: cx1, f: cy1 }] = go(
+    rangeL(3),
+    mapL((i) => index - 1 + i),
+    mapL((i) => transform_list.getItem(i)),
+    mapL(({ matrix: m }) => m)
+  );
+  const [x, y, width, height] = go(
+    [x_name, y_name, width_name, height_name],
+    mapL((name) => $el.getAttributeNS(null, name)),
+    mapL(parseFloat)
+  );
+
+  go(
+    [
+      [x, sx, cx1, cx2, width, ["e", "w"]],
+      [y, sy, cy1, cy2, height, ["n", "s"]],
+    ],
+    mapL(([v, s, c1, c2, l, conditions]) =>
+      go(
+        conditions,
+        some((condition) => direction.includes(condition)),
+        (is_changed) =>
+          is_changed
+            ? go1((v + c1) * s + c2, (v) =>
+                s < 0 && is_need_correction ? v + l * s : v
+              )
+            : v
+      )
+    ),
+    ([scaled_x, scaled_y]) => [
+      [x_name, scaled_x],
+      [y_name, scaled_y],
+      [width_name, width * Math.abs(sx)],
+      [height_name, height * Math.abs(sy)],
+    ],
+    mapL(([k, v]) => [k, `${v}`]),
+    each(([k, v]) => $el.setAttributeNS(null, k, v))
+  );
+
+  each(() => transform_list.removeItem(index - 1), rangeL(3));
+
+  return $el;
+};
+
+export const $$mergeScaleTransform23 = curry(
+  (
+    {
+      index = 1,
+      is_need_correction = true,
+      x_name = "x",
+      y_name = "y",
+      width_name = "width",
+      height_name = "height",
+      direction,
+    } = {},
+    $el
+  ) => {
+    const transform_list = $$getBaseTransformList($el);
+
+    if (
+      !$$isValidFxScaleSVGTransformList(transform_list, { index }) ||
+      !VALID_DIRECTION.has(direction)
+    ) {
+      return $el;
+    }
+
+    const [{ e: cx2, f: cy2 }, { a: sx, d: sy }, { e: cx1, f: cy1 }] = go(
+      rangeL(3),
+      mapL((i) => index - 1 + i),
+      mapL((i) => transform_list.getItem(i)),
+      mapL(({ matrix: m }) => m)
+    );
+    const [x, y, width, height] = go(
+      [x_name, y_name, width_name, height_name],
+      mapL((name) => $el.getAttributeNS(null, name)),
+      mapL(parseFloat)
+    );
+
+    go(
+      [
+        [x, sx, cx1, cx2, width, ["e", "w"]],
+        [y, sy, cy1, cy2, height, ["n", "s"]],
+      ],
+      mapL(([v, s, c1, c2, l, conditions]) =>
+        go(
+          conditions,
+          some((condition) => direction.includes(condition)),
+          (is_changed) =>
+            is_changed
+              ? go1((v + c1) * s + c2, (v) =>
+                  s < 0 && is_need_correction ? v + l * s : v
+                )
+              : v
+        )
+      ),
+      ([scaled_x, scaled_y]) => [
+        [x_name, scaled_x],
+        [y_name, scaled_y],
+        [width_name, width * Math.abs(sx)],
+        [height_name, height * Math.abs(sy)],
+      ],
+      mapL(([k, v]) => [k, `${v}`]),
+      each(([k, v]) => $el.setAttributeNS(null, k, v))
+    );
+
+    each(() => transform_list.removeItem(index - 1), rangeL(3));
+
+    return $el;
+  }
+);
