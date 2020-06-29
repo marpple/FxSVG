d453b33b1844d9b821685a67edfc87deb498ce48
daengdaenglee <gunho1020@gmail.com>
2020-06-29 16:20:46 +0900

    $$controlRotateTransform 함수 interface 수정 초안

--- src/controlRotateTransform/controlRotateTransform.index.js
+++ src/controlRotateTransform/controlRotateTransform.index.js
@@ -1,3 +1,4 @@
+import { curry } from "fxjs2";
 import { $$appendRotateTransform } from "../appendRotateTransform/appendRotateTransform.index.js";
 import { $$getSVG } from "../getSetSVG/getSetSVG.index.js";
 import { $$initRotateTransform } from "../initRotateTransform/initRotateTransform.index.js";
@@ -26,3 +27,55 @@ export const $$controlRotateTransform = ($svg = $$getSVG()) => (
 
   return { $el, transform, controller };
 };
+
+export const $$controlRotateTransform2 = ({
+  angle,
+  cx,
+  cy,
+  index = 0,
+} = {}) => ($el, $svg = $$getSVG()) => {
+  const transform = $$initRotateTransform($svg)($el, { angle, cx, cy, index });
+
+  const controller = {};
+  controller.update = ({ angle } = {}) => {
+    $$updateRotateTransform(transform, { angle, cx: 0, cy: 0 });
+    return controller;
+  };
+  controller.append = ({ angle } = {}) => {
+    $$appendRotateTransform(transform, { angle });
+    return controller;
+  };
+  controller.end = () => {
+    $$mergeRotateTransform($svg)($el, { index: index + 1 });
+    return $el;
+  };
+
+  return { $el, transform, controller };
+};
+
+export const $$controlRotateTransform3 = curry(
+  ({ angle, cx, cy, index = 0 } = {}, $el, $svg = $$getSVG()) => {
+    const transform = $$initRotateTransform($svg)($el, {
+      angle,
+      cx,
+      cy,
+      index,
+    });
+
+    const controller = {};
+    controller.update = ({ angle } = {}) => {
+      $$updateRotateTransform(transform, { angle, cx: 0, cy: 0 });
+      return controller;
+    };
+    controller.append = ({ angle } = {}) => {
+      $$appendRotateTransform(transform, { angle });
+      return controller;
+    };
+    controller.end = () => {
+      $$mergeRotateTransform($svg)($el, { index: index + 1 });
+      return $el;
+    };
+
+    return { $el, transform, controller };
+  }
+);
