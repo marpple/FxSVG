617bbe7ef010ea8784f076b955f44060b0b25124
daengdaenglee <gunho1020@gmail.com>
2020-06-29 16:16:51 +0900

    $$mergeTranslateTransform2 함수 interface 수정 초안

--- src/mergeTranslateTransform/mergeTranslateTransform.index.js
+++ src/mergeTranslateTransform/mergeTranslateTransform.index.js
@@ -1,4 +1,4 @@
-import { each, go, mapL, rangeL } from "fxjs2";
+import { curry, each, go, mapL, rangeL } from "fxjs2";
 import { $$createSVGTransformTranslate } from "../createSVGTransformTranslate/createSVGTransformTranslate.index.js";
 import { $$getBaseTransformList } from "../getBaseTransformList/getBaseTransformList.index.js";
 import { $$getSVG } from "../getSetSVG/getSetSVG.index.js";
@@ -50,3 +50,97 @@ export const $$mergeTranslateTransform = ($svg = $$getSVG()) => (
 
   return $el;
 };
+
+export const $$mergeTranslateTransform2 = ({
+  index = 0,
+  x_name = "x",
+  y_name = "y",
+} = {}) => ($el, $svg = $$getSVG()) => {
+  const base_tl = $$getBaseTransformList($el);
+  if (index > base_tl.numberOfItems - 1 || index < 0) {
+    return $el;
+  }
+
+  const transform = base_tl.getItem(index);
+  if (!$$isTranslateSVGTransform(transform)) {
+    return $el;
+  }
+
+  const { e: tx, f: ty } = transform.matrix;
+
+  go(
+    [
+      { name: x_name, value: tx },
+      { name: y_name, value: ty },
+    ],
+    mapL(({ name, value }) => ({
+      name,
+      value: `${parseFloat($el.getAttributeNS(null, name)) + value}`,
+    })),
+    each(({ name, value }) => $el.setAttributeNS(null, name, value))
+  );
+
+  base_tl.removeItem(index);
+
+  go(
+    rangeL(base_tl.numberOfItems),
+    mapL((i) => base_tl.getItem(i)),
+    mapL((t) => [
+      $$createSVGTransformTranslate($svg)({ tx, ty }),
+      t,
+      $$createSVGTransformTranslate($svg)({ tx: -tx, ty: -ty }),
+    ]),
+    each(([t1, t2, t3]) => {
+      const matrix = t1.matrix.multiply(t2.matrix).multiply(t3.matrix);
+      t2.setMatrix(matrix);
+    })
+  );
+
+  return $el;
+};
+
+export const $$mergeTranslateTransform3 = curry(
+  ({ index = 0, x_name = "x", y_name = "y" } = {}, $el, $svg = $$getSVG()) => {
+    const base_tl = $$getBaseTransformList($el);
+    if (index > base_tl.numberOfItems - 1 || index < 0) {
+      return $el;
+    }
+
+    const transform = base_tl.getItem(index);
+    if (!$$isTranslateSVGTransform(transform)) {
+      return $el;
+    }
+
+    const { e: tx, f: ty } = transform.matrix;
+
+    go(
+      [
+        { name: x_name, value: tx },
+        { name: y_name, value: ty },
+      ],
+      mapL(({ name, value }) => ({
+        name,
+        value: `${parseFloat($el.getAttributeNS(null, name)) + value}`,
+      })),
+      each(({ name, value }) => $el.setAttributeNS(null, name, value))
+    );
+
+    base_tl.removeItem(index);
+
+    go(
+      rangeL(base_tl.numberOfItems),
+      mapL((i) => base_tl.getItem(i)),
+      mapL((t) => [
+        $$createSVGTransformTranslate($svg)({ tx, ty }),
+        t,
+        $$createSVGTransformTranslate($svg)({ tx: -tx, ty: -ty }),
+      ]),
+      each(([t1, t2, t3]) => {
+        const matrix = t1.matrix.multiply(t2.matrix).multiply(t3.matrix);
+        t2.setMatrix(matrix);
+      })
+    );
+
+    return $el;
+  }
+);
