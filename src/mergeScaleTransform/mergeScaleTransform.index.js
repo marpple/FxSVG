8ee03fdcde31e0cd0bf82c359d19ebde1c536196
daengdaenglee <gunho1020@gmail.com>
2020-06-29 16:14:15 +0900

    $$mergeScaleTransform 함수 interface 수정 초안

--- src/mergeScaleTransform/mergeScaleTransform.index.js
+++ src/mergeScaleTransform/mergeScaleTransform.index.js
@@ -1,4 +1,4 @@
-import { each, go, mapL, rangeL, reduce } from "fxjs2";
+import { curry, each, go, mapL, rangeL, reduce } from "fxjs2";
 import { $$createSVGTransformMatrix } from "../createSVGTransformMatrix/createSVGTransformMatrix.index.js";
 import { $$getBaseTransformList } from "../getBaseTransformList/getBaseTransformList.index.js";
 import { $$getSVG } from "../getSetSVG/getSetSVG.index.js";
@@ -28,3 +28,52 @@ export const $$mergeScaleTransform = ($svg = $$getSVG()) => (
 
   return $el;
 };
+
+export const $$mergeScaleTransform2 = ({ index = 1 } = {}) => (
+  $el,
+  $svg = $$getSVG()
+) => {
+  const transform_list = $$getBaseTransformList($el);
+  if (!$$isValidFxScaleSVGTransformList(transform_list, { index })) {
+    return $el;
+  }
+
+  const merged_transform = go(
+    rangeL(3),
+    mapL((i) => index - 1 + i),
+    mapL((i) => transform_list.getItem(i)),
+    mapL(({ matrix }) => matrix),
+    reduce((m1, m2) => m1.multiply(m2)),
+    (matrix) => ({ matrix }),
+    $$createSVGTransformMatrix($svg)
+  );
+
+  each(() => transform_list.removeItem(index - 1), rangeL(3));
+  transform_list.insertItemBefore(merged_transform, index - 1);
+
+  return $el;
+};
+
+export const $$mergeScaleTransform3 = curry(
+  ({ index = 1 } = {}, $el, $svg = $$getSVG()) => {
+    const transform_list = $$getBaseTransformList($el);
+    if (!$$isValidFxScaleSVGTransformList(transform_list, { index })) {
+      return $el;
+    }
+
+    const merged_transform = go(
+      rangeL(3),
+      mapL((i) => index - 1 + i),
+      mapL((i) => transform_list.getItem(i)),
+      mapL(({ matrix }) => matrix),
+      reduce((m1, m2) => m1.multiply(m2)),
+      (matrix) => ({ matrix }),
+      $$createSVGTransformMatrix($svg)
+    );
+
+    each(() => transform_list.removeItem(index - 1), rangeL(3));
+    transform_list.insertItemBefore(merged_transform, index - 1);
+
+    return $el;
+  }
+);
