9aed710d127285507966c2bea7fa9c4e98933f9d
daengdaenglee <gunho1020@gmail.com>
2020-06-29 17:59:34 +0900

    test.client.cjs 스크립트 수정

--- bin/test.client.cjs
+++ bin/test.client.cjs
@@ -27,114 +27,124 @@ const runTest = async (
   return page.evaluate(evaluateTestResults);
 };
 
-const runTestsDefault = async ({ browser_type_list, total_count }) => {
-  const results = [];
-  for (const browser_type of browser_type_list) {
-    const browser = await playwright[browser_type].launch({
-      headless: false,
-    });
-    let fail_count = 0;
-    let success_count = 0;
-    for (let i = 0; i < total_count; i++) {
-      const context = await browser.newContext({ viewport: null });
-      const page = await context.newPage();
-      const is_success = await runTest(
-        {
-          url: TEST_PAGE_DEFAULT_URL,
-          waitForTests: () => window.FxSVG_TEST_FAILURES != null,
-          evaluateTestResults: () => window.FxSVG_TEST_FAILURES === 0,
-        },
-        page
-      );
-      if (is_success) {
-        await page.close();
-        success_count += 1;
-      } else {
-        fail_count += 1;
-      }
-
-      console.log(
-        `${browser_type}::[success:${success_count}][fail:${fail_count}][total:${total_count}]`
-      );
-    }
-
-    if (fail_count === 0) {
-      console.log(`${browser_type}::ALL_SUCCESS`);
-      await browser.close();
+const runTests = async function* ({
+  browser_type,
+  total_count,
+  headless,
+  url,
+  waitForTests,
+  evaluateTestResults,
+  checkIsSuccess,
+}) {
+  const browser = await playwright[browser_type].launch({ headless });
+  let [success_count, fail_count] = [0, 0];
+  for (let i = 0; i < total_count; i++) {
+    const context = await browser.newContext({ viewport: null });
+    const page = await context.newPage();
+    const test_results = await runTest(
+      { url, waitForTests, evaluateTestResults },
+      page
+    );
+    if (checkIsSuccess(test_results)) {
+      success_count += 1;
     } else {
-      console.log(`${browser_type}::FAIL`);
+      fail_count += 1;
     }
-
-    results.push({
-      browser_type,
+    yield {
       total: total_count,
       success: success_count,
       fail: fail_count,
-    });
+      test_results,
+      page,
+    };
   }
+  return {
+    total: total_count,
+    success: success_count,
+    fail: fail_count,
+    browser,
+  };
+};
 
+const runTestsDefault = async ({ browser_type_list, total_count }) => {
+  const reports = [];
+  for (const browser_type of browser_type_list) {
+    const iter = runTests({
+      browser_type,
+      total_count,
+      headless: false,
+      url: TEST_PAGE_DEFAULT_URL,
+      waitForTests: () => window.FxSVG_TEST_FAILURES != null,
+      evaluateTestResults: () => window.FxSVG_TEST_FAILURES === 0,
+      checkIsSuccess: (is_success) => is_success,
+    });
+    let cur;
+    while (((cur = await iter.next()), !cur.done)) {
+      const {
+        total,
+        success,
+        fail,
+        test_results: is_success,
+        page,
+      } = cur.value;
+      is_success && (await page.close());
+      console.log(
+        `${browser_type}::[success:${success}][fail:${fail}][total:${total}]`
+      );
+    }
+    const { total, success, fail, browser } = cur.value;
+    reports.push({ browser_type, total, success, fail });
+    !fail && (await browser.close());
+  }
+  return reports;
+};
+
+const reportTestsDefault = (reports) => {
   console.log("=== REPORT ===");
-  console.log(
-    results
-      .map(
-        ({ browser_type, total, success, fail }) =>
-          `${browser_type}::[success:${success}][fail:${fail}][total:${total}]`
-      )
-      .join("\n")
-  );
+  for (const { browser_type, total, success, fail } of reports) {
+    console.log(
+      `${browser_type}::[success:${success}][fail:${fail}][total:${total}]`
+    );
+  }
 };
 
 const runTestsJSON = async ({ browser_type_list, total_count }) => {
   const reports = [];
-
   for (const browser_type of browser_type_list) {
-    const browser = await playwright[browser_type].launch({
-      headless: true,
-    });
-    let fail_count = 0;
-    let success_count = 0;
     const results = [];
-    for (let i = 0; i < total_count; i++) {
-      const context = await browser.newContext({ viewport: null });
-      const page = await context.newPage();
-      const test_results = await runTest(
-        {
-          url: TEST_PAGE_JSON_URL,
-          waitForTests: () => window.FxSVG_TEST_RESULTS_JSON != null,
-          evaluateTestResults: () => window.FxSVG_TEST_RESULTS_JSON,
-        },
-        page
-      );
+    const iter = runTests({
+      browser_type,
+      total_count,
+      headless: true,
+      url: TEST_PAGE_JSON_URL,
+      waitForTests: () => window.FxSVG_TEST_RESULTS_JSON != null,
+      evaluateTestResults: () => window.FxSVG_TEST_RESULTS_JSON,
+      checkIsSuccess: ({ stats: { failures } }) => failures === 0,
+    });
+    let cur;
+    while (((cur = await iter.next()), !cur.done)) {
+      const { total, success, fail, test_results, page } = cur.value;
       await page.close();
       results.push(test_results);
-
-      if (test_results.stats.failures === 0) {
-        success_count += 1;
-      } else {
-        fail_count += 1;
-      }
-
       console.log(
-        `${browser_type}::[success:${success_count}][fail:${fail_count}][total:${total_count}]`
+        `${browser_type}::[success:${success}][fail:${fail}][total:${total}]`
       );
     }
-
+    const { total, success, fail, browser } = cur.value;
     await browser.close();
-    if (fail_count === 0) {
-      console.log(`${browser_type}::ALL_SUCCESS`);
-    } else {
-      console.log(`${browser_type}::FAIL`);
-    }
-
     reports.push({
       browser_type,
-      total: total_count,
-      success: success_count,
-      fail: fail_count,
+      total,
+      success,
+      fail,
       results,
     });
+    console.log(`${browser_type}::${fail === 0 ? "ALL_SUCCESS" : "FAIL"}`);
   }
+  return reports;
+};
 
+const reportTestsJSON = (reports) => {
   console.log("=== REPORT ===");
   for (const { browser_type, total, success, fail, results } of reports) {
     console.group(
@@ -157,23 +167,27 @@ const runTestsJSON = async ({ browser_type_list, total_count }) => {
 
     console.groupEnd();
   }
-
-  if (reports.map(({ fail }) => fail).reduce((a, b) => a + b, 0) > 0) {
-    process.exit(1);
-  }
 };
 
-(async () => {
+const main = async () => {
   if (TEST_HEADLESS) {
-    await runTestsJSON({
+    const reports = await runTestsJSON({
       browser_type_list: BROWSER_TYPE_LIST,
       total_count: TEST_REPEAT_COUNT,
     });
+    reportTestsJSON(reports);
+    const total_fail = reports
+      .map(({ fail }) => fail)
+      .reduce((a, b) => a + b, 0);
+    total_fail > 0 && process.exit(1);
     return;
   }
 
-  await runTestsDefault({
+  const reports = await runTestsDefault({
     browser_type_list: BROWSER_TYPE_LIST,
     total_count: TEST_REPEAT_COUNT,
   });
-})();
+  reportTestsDefault(reports);
+};
+
+main();
