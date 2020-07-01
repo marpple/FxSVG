992942b4d786e3ae01d1e4f7ab3fb9687f710b1e
daengdaenglee <gunho1020@gmail.com>
2020-07-01 18:08:26 +0900

    $$initTranslateTransform test code 수정

--- src/initTranslateTransform/initTranslateTransform.spec.js
+++ src/initTranslateTransform/initTranslateTransform.spec.js
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { each, equals2, go, mapL, rejectL, zipL, zipWithIndexL } from "fxjs2";
+import { equals2, go, mapL, rejectL, zipL, zipWithIndexL } from "fxjs2";
 import {
   deepCopyTransformList,
   makeMockRect,
@@ -79,16 +79,16 @@ export default ({ describe, it }) => [
         expect(after_transform_list.length).equal(
           before_transform_list.length + 1
         );
-        go(
+        const pairs = go(
           after_transform_list,
           zipWithIndexL,
           rejectL(([i]) => equals2(index, i)),
           mapL(([, transform]) => transform),
-          zipL(before_transform_list),
-          each(([before_transform, after_transform]) =>
-            expectSameValueSVGTransform(after_transform, before_transform)
-          )
+          zipL(before_transform_list)
         );
+        for (const [before_transform, after_transform] of pairs) {
+          expectSameValueSVGTransform(after_transform, before_transform);
+        }
       }
     });
 
