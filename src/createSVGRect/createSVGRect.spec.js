3b7dc97fb87f05325d24b50830482524cd290749
daengdaenglee <gunho1020@gmail.com>
2020-07-01 16:49:44 +0900

    $$createSVGRect test code 수정

--- src/createSVGRect/createSVGRect.spec.js
+++ src/createSVGRect/createSVGRect.spec.js
@@ -1,15 +1,5 @@
 import { expect } from "chai";
-import {
-  appendL,
-  defaultTo,
-  each,
-  extend,
-  flatMapL,
-  go,
-  mapL,
-  object,
-  pipe,
-} from "fxjs2";
+import { appendL, defaultTo, extend, flatMapL, go, mapL, object } from "fxjs2";
 import { makeAllCombinations, makeRandomInt } from "../../test/utils/index.js";
 import { $$createSVGRect } from "./createSVGRect.index.js";
 
@@ -17,12 +7,8 @@ const makeCases = () =>
   go(
     ["x", "y", "width", "height"],
     makeAllCombinations,
-    mapL(
-      pipe(
-        mapL((k) => [k, makeRandomInt()]),
-        object
-      )
-    ),
+    mapL(mapL((k) => [k, makeRandomInt()])),
+    mapL(object),
     mapL((values) => ({ values, f: $$createSVGRect(values) })),
     appendL({ f: $$createSVGRect() }),
     flatMapL(({ values, f }) =>
@@ -36,33 +22,24 @@ const makeCases = () =>
 export default ({ describe, it }) => [
   describe(`$$createSVGRect`, function () {
     it(`The return value is a SVGRect.`, function () {
-      go(
-        makeCases(),
-        mapL(({ rect: r }) => r),
-        each((r) => expect(r).instanceof(SVGRect))
-      );
+      for (const { rect } of makeCases()) {
+        expect(rect).instanceof(SVGRect);
+      }
     });
 
     it(`Each value of the rect will be same with the given value.
         If there is omitted values or no argument,
         the values will be {x: 0, y: 0, width: 0, height: 0} individually by default.`, function () {
-      go(
-        makeCases(),
-        mapL(({ rect, values }) =>
-          go(
-            values,
-            defaultTo({}),
-            (values) => extend({ x: 0, y: 0, width: 0, height: 0 }, values),
-            (values) => ({ rect, values })
-          )
-        ),
-        each(({ rect, values }) => {
-          expect(rect.x).equal(values.x);
-          expect(rect.y).equal(values.y);
-          expect(rect.width).equal(values.width);
-          expect(rect.height).equal(values.height);
-        })
-      );
+      for (const { rect, values } of makeCases()) {
+        const expect_values = extend(
+          { x: 0, y: 0, width: 0, height: 0 },
+          defaultTo({}, values)
+        );
+        expect(rect.x).equal(expect_values.x);
+        expect(rect.y).equal(expect_values.y);
+        expect(rect.width).equal(expect_values.width);
+        expect(rect.height).equal(expect_values.height);
+      }
     });
   }),
 ];
