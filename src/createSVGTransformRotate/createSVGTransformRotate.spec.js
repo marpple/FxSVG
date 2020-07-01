f0d0bf8463be3c2722b982bfb361b029700a690a
daengdaenglee <gunho1020@gmail.com>
2020-07-01 16:56:50 +0900

    $$createSVGTransformRotate test code 수정

--- src/createSVGTransformRotate/createSVGTransformRotate.spec.js
+++ src/createSVGTransformRotate/createSVGTransformRotate.spec.js
@@ -1,16 +1,5 @@
 import { expect } from "chai";
-import {
-  appendL,
-  defaultTo,
-  each,
-  extend,
-  flatMapL,
-  go,
-  mapL,
-  object,
-  pipe,
-  tap,
-} from "fxjs2";
+import { appendL, defaultTo, extend, flatMapL, go, mapL, object } from "fxjs2";
 import { expectSameValueSVGTransform } from "../../test/assertions/index.js";
 import {
   makeAllCombinations,
@@ -24,12 +13,8 @@ const makeCases = () =>
   go(
     ["angle", "cx", "cy"],
     makeAllCombinations,
-    mapL(
-      pipe(
-        mapL((k) => [k, makeRandomNumber(-100, 100)]),
-        object
-      )
-    ),
+    mapL(mapL((k) => [k, makeRandomNumber(-100, 100)])),
+    mapL(object),
     mapL((values) => ({ values, f: $$createSVGTransformRotate(values) })),
     appendL({ f: $$createSVGTransformRotate() }),
     flatMapL(({ values, f }) =>
@@ -43,45 +28,29 @@ const makeCases = () =>
 export default ({ describe, it }) => [
   describe(`$$createSVGTransformRotate`, function () {
     it(`The return value is a SVGTransform.`, function () {
-      go(
-        makeCases(),
-        mapL(({ transform: t }) => t),
-        each((t) => expect(t).instanceof(SVGTransform))
-      );
+      for (const { transform } of makeCases()) {
+        expect(transform).instanceof(SVGTransform);
+      }
     });
 
     it(`The transform's type is the SVGTransform.SVG_TRANSFORM_ROTATE.`, function () {
-      go(
-        makeCases(),
-        mapL(({ transform: t }) => t),
-        each((transform) => expect($$isRotateSVGTransform(transform)).true)
-      );
+      for (const { transform } of makeCases()) {
+        expect($$isRotateSVGTransform(transform)).true;
+      }
     });
 
     it(`The transform is initialized with the given angle, cx, cy.
         If there are omitted values or no argument,
         the values will be {angle: 0, cx: 0, cy: 0} individually by default.`, function () {
-      go(
-        makeCases(),
-        mapL(({ transform, values }) =>
-          go(
-            values,
-            defaultTo({}),
-            (values) => extend({ angle: 0, cx: 0, cy: 0 }, values),
-            (values) => ({ values, transform })
-          )
-        ),
-        mapL(({ transform: receive_transform, values: { angle, cx, cy } }) =>
-          go(
-            $$createSVGTransform(),
-            tap((transform) => transform.setRotate(angle, cx, cy)),
-            (expect_transform) => ({ receive_transform, expect_transform })
-          )
-        ),
-        each(({ receive_transform, expect_transform }) =>
-          expectSameValueSVGTransform(receive_transform, expect_transform)
-        )
-      );
+      for (const { transform, values } of makeCases()) {
+        const { angle, cx, cy } = extend(
+          { angle: 0, cx: 0, cy: 0 },
+          defaultTo({}, values)
+        );
+        const expect_transform = $$createSVGTransform();
+        expect_transform.setRotate(angle, cx, cy);
+        expectSameValueSVGTransform(transform, expect_transform);
+      }
     });
   }),
 ];
