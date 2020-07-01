db5851ae1edd5cc304175278a285718c98e75aab
daengdaenglee <gunho1020@gmail.com>
2020-07-01 16:40:36 +0900

    $$createSVGTransformTranslate 구현 수정

--- src/createSVGTransformTranslate/createSVGTransformTranslate.index.js
+++ src/createSVGTransformTranslate/createSVGTransformTranslate.index.js
@@ -1,10 +1,10 @@
-import { tap } from "fxjs2";
 import { $$createSVGTransform } from "../createSVGTransform/createSVGTransform.index.js";
 import { $$getSVG } from "../getSetSVG/getSetSVG.index.js";
 
 export const $$createSVGTransformTranslate = ({ tx = 0, ty = 0 } = {}) => (
   $svg = $$getSVG()
-) =>
-  tap((transform) => transform.setTranslate(tx, ty))(
-    $$createSVGTransform($svg)
-  );
+) => {
+  const transform = $$createSVGTransform($svg);
+  transform.setTranslate(tx, ty);
+  return transform;
+};
