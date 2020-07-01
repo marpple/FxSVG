96ad75f0261ba6d9d75023708bfb3ff79431a619
daengdaenglee <gunho1020@gmail.com>
2020-07-01 16:39:52 +0900

    $$createSVGTransformScale 구현 수정

--- src/createSVGTransformScale/createSVGTransformScale.index.js
+++ src/createSVGTransformScale/createSVGTransformScale.index.js
@@ -1,7 +1,10 @@
-import { tap } from "fxjs2";
 import { $$createSVGTransform } from "../createSVGTransform/createSVGTransform.index.js";
 import { $$getSVG } from "../getSetSVG/getSetSVG.index.js";
 
 export const $$createSVGTransformScale = ({ sx = 1, sy = 1 } = {}) => (
   $svg = $$getSVG()
-) => tap((transform) => transform.setScale(sx, sy))($$createSVGTransform($svg));
+) => {
+  const transform = $$createSVGTransform($svg);
+  transform.setScale(sx, sy);
+  return transform;
+};
