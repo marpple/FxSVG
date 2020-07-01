7065530e38f672a6368ad9faee7cae189166a74c
daengdaenglee <gunho1020@gmail.com>
2020-07-01 16:33:47 +0900

    $$createSVGPoint 구현 수정

--- src/createSVGPoint/createSVGPoint.index.js
+++ src/createSVGPoint/createSVGPoint.index.js
@@ -1,10 +1,10 @@
-import { tap } from "fxjs2";
 import { $$getSVG } from "../getSetSVG/getSetSVG.index.js";
 
 export const $$createSVGPoint = ({ x = 0, y = 0 } = {}) => (
   $svg = $$getSVG()
-) =>
-  tap((p) => {
-    p.x = x;
-    p.y = y;
-  })($svg.createSVGPoint());
+) => {
+  const point = $svg.createSVGPoint();
+  point.x = x;
+  point.y = y;
+  return point;
+};
