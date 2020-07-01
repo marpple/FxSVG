a25f6a1a79a9d317a815650a85837a2e4622a0d5
daengdaenglee <gunho1020@gmail.com>
2020-07-01 16:47:43 +0900

    $$createSVGPoint README 수정

--- src/createSVGPoint/createSVGPoint.spec.js
+++ src/createSVGPoint/createSVGPoint.spec.js
@@ -1,15 +1,5 @@
 import { expect } from "chai";
-import {
-  appendL,
-  defaultTo,
-  each,
-  extend,
-  flatMapL,
-  go,
-  mapL,
-  object,
-  pipe,
-} from "fxjs2";
+import { appendL, defaultTo, extend, flatMapL, go, mapL, object } from "fxjs2";
 import { makeAllCombinations, makeRandomInt } from "../../test/utils/index.js";
 import { $$createSVGPoint } from "./createSVGPoint.index.js";
 
@@ -17,12 +7,8 @@ const makeCases = () =>
   go(
     ["x", "y"],
     makeAllCombinations,
-    mapL(
-      pipe(
-        mapL((k) => [k, makeRandomInt(-100, 100)]),
-        object
-      )
-    ),
+    mapL(mapL((k) => [k, makeRandomInt(-100, 100)])),
+    mapL(object),
     mapL((values) => ({ values, f: $$createSVGPoint(values) })),
     appendL({ f: $$createSVGPoint() }),
     flatMapL(({ values, f }) =>
@@ -36,31 +22,19 @@ const makeCases = () =>
 export default ({ describe, it }) => [
   describe(`$$createSVGPoint`, function () {
     it(`The return value is a SVGPoint.`, function () {
-      go(
-        makeCases(),
-        mapL(({ point: p }) => p),
-        each((p) => expect(p).instanceof(SVGPoint))
-      );
+      for (const { point } of makeCases()) {
+        expect(point).instanceof(SVGPoint);
+      }
     });
 
     it(`Each value of the point will be same with the given value.
         If there is omitted values or no argument,
         the values will be {x: 0, y: 0} individually by default.`, function () {
-      go(
-        makeCases(),
-        mapL(({ point, values }) =>
-          go(
-            values,
-            defaultTo({}),
-            (values) => extend({ x: 0, y: 0 }, values),
-            (values) => ({ point, values })
-          )
-        ),
-        each(({ point, values }) => {
-          expect(point.x).equal(values.x);
-          expect(point.y).equal(values.y);
-        })
-      );
+      for (const { point, values } of makeCases()) {
+        const expect_values = extend({ x: 0, y: 0 }, defaultTo({}, values));
+        expect(point.x).equal(expect_values.x);
+        expect(point.y).equal(expect_values.y);
+      }
     });
   }),
 ];
