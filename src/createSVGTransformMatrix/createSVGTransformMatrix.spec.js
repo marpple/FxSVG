e083e64475f9900fc34b4a37776c29d94246559c
daengdaenglee <gunho1020@gmail.com>
2020-07-01 16:54:04 +0900

    $$createSVGTransformMatrix test code 수정

--- src/createSVGTransformMatrix/createSVGTransformMatrix.spec.js
+++ src/createSVGTransformMatrix/createSVGTransformMatrix.spec.js
@@ -1,15 +1,5 @@
 import { expect } from "chai";
-import {
-  appendL,
-  defaultTo,
-  each,
-  extend,
-  flatMapL,
-  go,
-  mapL,
-  object,
-  pipe,
-} from "fxjs2";
+import { appendL, defaultTo, extend, flatMapL, go, mapL, object } from "fxjs2";
 import { expectSameValueSVGMatrix } from "../../test/assertions/index.js";
 import {
   makeAllCombinations,
@@ -25,14 +15,9 @@ const makeCases = () =>
     ["matrix"],
     makeAllCombinations,
     mapL(
-      pipe(
-        mapL((k) => [
-          k,
-          makeRandomSVGMatrix(() => makeRandomNumber(-100, 100)),
-        ]),
-        object
-      )
+      mapL((k) => [k, makeRandomSVGMatrix(() => makeRandomNumber(-100, 100))])
     ),
+    mapL(object),
     mapL((values) => ({ values, f: $$createSVGTransformMatrix(values) })),
     appendL({ f: $$createSVGTransformMatrix() }),
     flatMapL(({ values, f }) =>
@@ -46,37 +31,26 @@ const makeCases = () =>
 export default ({ describe, it }) => [
   describe(`$$createSVGTransformMatrix`, function () {
     it(`The return value is a SVGTransform.`, function () {
-      go(
-        makeCases(),
-        mapL(({ transform: t }) => t),
-        each((transform) => expect(transform).instanceof(SVGTransform))
-      );
+      for (const { transform } of makeCases()) {
+        expect(transform).instanceof(SVGTransform);
+      }
     });
 
     it(`The transform is a matrix transform.`, function () {
-      go(
-        makeCases(),
-        mapL(({ transform: t }) => t),
-        each((transform) => expect($$isMatrixSVGTransform(transform)).true)
-      );
+      for (const { transform } of makeCases()) {
+        expect($$isMatrixSVGTransform(transform)).true;
+      }
     });
 
     it(`The transform is initialized with the given matrix.
         If there is omitted values or no argument, the transform is initialized with an identity matrix.`, function () {
-      go(
-        makeCases(),
-        mapL(({ transform, values }) =>
-          go(
-            values,
-            defaultTo({}),
-            (values) => extend({ matrix: $$createSVGMatrix()() }, values),
-            (values) => ({ values, transform })
-          )
-        ),
-        each(({ transform, values: { matrix } }) =>
-          expectSameValueSVGMatrix(transform.matrix, matrix)
-        )
-      );
+      for (const { transform, values } of makeCases()) {
+        const { matrix } = extend(
+          { matrix: $$createSVGMatrix()() },
+          defaultTo({}, values)
+        );
+        expectSameValueSVGMatrix(transform.matrix, matrix);
+      }
     });
   }),
 ];
