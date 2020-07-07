0e09338604003d2787714224ed1c3dc2b30dbfde
daengdaenglee <gunho1020@gmail.com>
2020-07-07 13:55:04 +0900

    $$append, $$merge 메소드 추가

--- src/LiveTransform/LiveTransform.index.js
+++ src/LiveTransform/LiveTransform.index.js
@@ -12,7 +12,21 @@ export const $$LiveTransform = type(
         return $$update(option)(live_transform);
       };
     }
+
+    $$append(option) {
+      return (live_transform) => {
+        const { $$append } = this(live_transform);
+        return $$append(option)(live_transform);
+      };
+    }
+
+    $$merge(option) {
+      return (live_transform) => {
+        const { $$merge } = this(live_transform);
+        return $$merge(option)(live_transform);
+      };
+    }
   }
 );
 
-export const { $$update } = $$LiveTransform.prototype;
+export const { $$update, $$append, $$merge } = $$LiveTransform.prototype;
--- src/LiveTranslateTransform/LiveTranslateTransform.index.js
+++ src/LiveTranslateTransform/LiveTranslateTransform.index.js
@@ -1,6 +1,9 @@
+import { defaultTo } from "fxjs2";
+import { $$appendTranslateTransform } from "../appendTranslateTransform/appendTranslateTransform.index.js";
 import { $$getSVG } from "../getSetSVG/getSetSVG.index.js";
 import { $$initTranslateTransform } from "../initTranslateTransform/initTranslateTransform.index.js";
 import { $$LiveTransform } from "../LiveTransform/LiveTransform.index.js";
+import { $$mergeTranslateTransform } from "../mergeTranslateTransform/mergeTranslateTransform.index.js";
 import { $$updateTranslateTransform } from "../updateTranslateTransform/updateTranslateTransform.index.js";
 
 export class $$LiveTranslateTransform {
@@ -13,11 +16,22 @@ export class $$LiveTranslateTransform {
     $el,
     $svg = $$getSVG()
   ) {
-    this.transform = $$initTranslateTransform({ tx, ty, index })($el, $svg);
-    this.x_name = x_name;
-    this.y_name = y_name;
-    this.index = index;
-    this.$svg = $svg;
+    const kvs = [
+      ["transform", $$initTranslateTransform({ tx, ty, index })($el, $svg)],
+      ["x_name", x_name],
+      ["y_name", y_name],
+      ["index", index],
+      ["$el", $el],
+      ["$svg", $svg],
+    ];
+    for (const [key, value] of kvs) {
+      Object.defineProperty(this, key, {
+        value,
+        enumerable: false,
+        configurable: false,
+        writable: false,
+      });
+    }
   }
 
   static from({ index = 0, tx = 0, ty = 0, x_name, y_name } = {}) {
@@ -31,11 +45,35 @@ export class $$LiveTranslateTransform {
 }
 
 $$LiveTransform.instance($$LiveTranslateTransform, {
-  $$update({ tx, ty }) {
+  $$update({ tx, ty } = {}) {
     return (live_transform) => {
       const { transform } = live_transform;
       $$updateTranslateTransform({ tx, ty })(transform);
       return live_transform;
+    };
+  },
+  $$append({ tx = 0, ty = 0 } = {}) {
+    return (live_transform) => {
+      const { transform } = live_transform;
+      $$appendTranslateTransform({ tx, ty })(transform);
+      return live_transform;
+    };
+  },
+  $$merge({ x_name: x_name1, y_name: y_name1 }) {
+    return (live_transform) => {
+      const {
+        x_name: x_name2,
+        y_name: y_name2,
+        index,
+        $el,
+        $svg,
+      } = live_transform;
+      const x_name = defaultTo(x_name2, x_name1);
+      const y_name = defaultTo(y_name2, y_name1);
+      if (x_name && y_name) {
+        $$mergeTranslateTransform({ index, x_name, y_name })($el, $svg);
+      }
+      return $el;
     };
   },
 });
