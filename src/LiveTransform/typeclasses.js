a4655ed6b16a0b3936f84ae2ccf30035b6ff0dc2
daengdaenglee <gunho1020@gmail.com>
2020-07-07 12:00:00 +0900

    typeclasses.js 보완

--- src/LiveTransform/typeclasses.js
+++ src/LiveTransform/typeclasses.js
@@ -1,7 +1,8 @@
 import { each, equals2, filterL, go, not } from "fxjs2";
 
-const VERSION = 0;
 let uniqueTag = 0;
+
+const $$getMethods = Symbol(`$$fxsvg/LiveTransform/typeclasses/getMethods`);
 
 export function type(Class) {
   const name = Class.name;
@@ -10,26 +11,32 @@ export function type(Class) {
     throw new Error(`invalid typeclass name: ${name}`);
   }
 
-  const symbolName = `$$fxsvg-${VERSION}/${name}/${uniqueTag++}`;
+  const symbolName = `$$fxsvg/LiveTransform/typeclasses/${name}/${uniqueTag++}`;
   const symbol = Symbol[symbolName]
     ? Symbol[symbolName]
     : (Symbol[symbolName] = Symbol(symbolName));
 
-  Class.for = function _for(value) {
-    let methods = value[symbol];
-    if (!methods) {
-      throw new Error(`No instance found on ${value} of typeclass ${name}`);
-    }
-    return methods;
-  };
+  Object.defineProperty(Class, $$getMethods, {
+    value(value) {
+      const methods = value[symbol];
+      if (!methods) {
+        throw new Error(`No instance found on ${value} of typeclass ${name}`);
+      }
+      return methods;
+    },
+    configurable: false,
+    enumerable: false,
+    writable: false,
+  });
 
   Class.instance = function (constructor, methods) {
     Object.defineProperty(constructor.prototype, symbol, {
       value: methods,
       configurable: true,
-      // make the prototype non-enumerable to prevent it from showing in Safari debugger
       enumerable: false,
+      writable: false,
     });
+    return constructor;
   };
 
   Class.symbolName = symbolName;
@@ -39,7 +46,10 @@ export function type(Class) {
   go(
     Object.keys(properties),
     filterL((key) => not(equals2(key, "constructor"))),
-    each((key) => (Class.prototype[key] = Class.prototype[key].bind(Class.for)))
+    each(
+      (key) =>
+        (Class.prototype[key] = Class.prototype[key].bind(Class[$$getMethods]))
+    )
   );
 
   return Class;
