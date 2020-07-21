e97a72adb79bd2ff3c57ae831dfb378cabd04acc
daengdaenglee <gunho1020@gmail.com>
2020-07-21 12:07:13 +0900

    테스트 서버 포트 옵션 받을 수 있게 수정

--- test/server.cjs
+++ test/server.cjs
@@ -2,6 +2,10 @@ const path = require("path");
 const express = require("express");
 const webpack = require("webpack");
 const middleware = require("webpack-dev-middleware");
+const yargs = require("yargs");
+
+const argv = yargs.alias("p", "port").default("port", "8080").parse();
+const port = parseInt(argv.port, 10);
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, "../")));
@@ -23,4 +27,4 @@ app.use(
 );
 app.get("/", (req, res) => res.redirect("/test"));
 
-app.listen(8080, () => console.log("TEST SERVER LISTENING ON 8080 PORT"));
+app.listen(port, () => console.log(`TEST SERVER LISTENING ON ${port} PORT`));
