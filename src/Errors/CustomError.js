d0dcd1ee7f6ad40ffcf0fdde98d75ad4c0e2ce9f
daengdaenglee <gunho1020@gmail.com>
2020-12-16 15:31:56 +0900

    Error class 주석 수정

--- src/Errors/CustomError.js
+++ src/Errors/CustomError.js
@@ -6,8 +6,9 @@
  */
 export class CustomError extends Error {
   /**
+   *
    * @param {string} [fn_path="<anonymous>"] - function name or path that the error is occurred.
-   * @param {?string} description
+   * @param {string} [description]
    */
   constructor(fn_path = "<anonymous>", description) {
     const message = `Failed to execute '${fn_path}' (FxSVG)${
